1d091c82f33891535cf759f5179f4a8206a8c847
Gwilom <92596982+NotADucc@users.noreply.github.com>
2024-12-02 23:04:28 +0100

    refactor: linqified day2 even more

--- day2.cs
+++ day2.cs
@@ -4,36 +4,34 @@ public class Program
     {
         const string file_name = @"";
 
-        bool CheckSafety(List<int> lst, Func<int, int, bool> pred) => lst
-            .Zip(lst.Skip(1))
-            .All(x => x.First != x.Second && pred(x.First, x.Second));
-
-        Func<int, int, bool> GivePredicate(bool input) => input
+        bool CheckSafety(List<int> lst)
+        {
+            Func<int, int, bool> pred = lst[0] < lst[^1]
                 ? (prev, curr) => prev < curr && prev + 3 >= curr
                 : (prev, curr) => prev > curr && prev - 3 <= curr;
 
-        int res_1 = 0, res_2 = 0;
-
-        foreach (string line in File.ReadAllLines(file_name))
-        {
-            var lst = line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse)
-                .ToList();
-
-            res_1 += CheckSafety(lst, GivePredicate(lst[0] < lst[^1])) ? 1 : 0;
-            res_2 += lst
-                .Select((_, index) => index)
-                .Where(index =>
-                {
-                    var tempList = lst.Where((_, idx) => idx != index).ToList();
-
-                    return CheckSafety(tempList, GivePredicate(tempList[0] < tempList[^1]));
-                })
-                .Any() ? 1 : 0;
+            return lst
+                .Zip(lst.Skip(1))
+                .All(x => x.First != x.Second && pred(x.First, x.Second));
         }
 
-        Console.WriteLine($"Res 1 : {res_1}");
-        Console.WriteLine($"Res 2 : {res_2}");
+        var res = File.ReadAllLines(file_name)
+            .Select(line =>
+            {
+                var lst = line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+
+                int res_1 = CheckSafety(lst) ? 1 : 0;
+                int res_2 = res_1 == 1 ? 1 : lst
+                    .Select((_, index) => index)
+                    .Where(index => CheckSafety(lst.Where((_, idx) => idx != index).ToList()))
+                    .Any() ? 1 : 0;
+
+                return new { res_1, res_2 };
+            }).ToArray();
+
+        Console.WriteLine($"Res 1 : {res.Sum(x => x.res_1)}");
+        Console.WriteLine($"Res 2 : {res.Sum(x => x.res_2)}");
     }
 }
-
