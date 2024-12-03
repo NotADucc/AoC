8adb8e26019d3d62530cf57e9d5ffea5aa986ac8
Gwilom <92596982+NotADucc@users.noreply.github.com>
2024-12-03 13:46:09 +0100

    refactor: day3 matches select to aggregate

--- day3.cs
+++ day3.cs
@@ -5,30 +5,30 @@ public class Program
     private static void Main(string[] args)
     {
         const string file_name = @"";
-        Regex rgx = new Regex("mul[(]\\d{1,3},\\d{1,3}[)]|do[(][)]|don't[(][)]");
+        Regex rgx = new Regex("mul[(]\\d{1,3},\\d{1,3}[)]|do[(][)]|don't[(][)]", RegexOptions.Compiled);
         bool mul = true;
 
         var res = File.ReadAllLines(file_name)
             .Select(line =>
                 rgx.Matches(line)
-                    .Select(x =>
+                    .Aggregate((0L, 0L), (curr, next) =>
                     {
-                        var split = x.Value.Split(',');
+                        var split = next.Value.Split(',');
                         if (split.Length == 2)
                         {
                             long res = long.Parse(split[0].Substring(4)) * int.Parse(split[1].Substring(0, split[1].Length - 1));
-                            return new { res_1 = res, res_2 = mul ? res : 0L };
+                            curr.Item1 += res;
+                            curr.Item2 += mul ? res : 0L;
                         }
-                        else 
+                        else
                         {
-                            mul = x.Value.Equals("do()");
-                            return new { res_1 = 0L, res_2 = 0L };
+                            mul = next.Value.Equals("do()");
                         }
+                        return curr;
                     })
-            ).SelectMany(x => x)
-            .ToArray();
+            ).ToArray();
 
-        Console.WriteLine($"Res 1 : {res.Sum(x => x.res_1)}");
-        Console.WriteLine($"Res 2 : {res.Sum(x => x.res_2)}");
+        Console.WriteLine($"Res 1 : {res.Sum(x => x.Item1)}");
+        Console.WriteLine($"Res 2 : {res.Sum(x => x.Item2)}");
     }
 }
