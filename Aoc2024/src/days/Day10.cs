999799954d7cc672fa811f1cf6a01b41bf82e4ea
Guillaume Desktop <robertohelper@outlook.com>
2024-12-10 11:51:05 +0100

    feature: added base for day 10

---
+++ Aoc2024/src/days/Day10.cs
@@ -0,0 +1,12 @@
+using AoC;
+
+public class Day10 : IRun
+{
+    public (long, long) Run()
+    {
+        string file_name = Path.Combine(Helper.GetFilesDir(), "aoc10.txt");
+        long res_1 = 0, res_2 = 0;
+        
+        return (res_1, res_2);
+    }
+}
