56e9031a24379e23a23191add9b4f04beea4191f
Guillaume Laptop <robertohelper@outlook.com>
2024-12-11 09:27:57 +0100

    feature: added day 11 test

---
+++ Aoc2024/testing/Day11Tests.cs
@@ -0,0 +1,24 @@
+﻿namespace AoCTesting
+{
+    public class Day11Test
+    {
+        private Day11 proj = new();
+        private (long res_1, long res_2) res;
+        public Day11Test()
+        {
+            res = proj.Run();
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            Assert.Equal(187738, res.res_1);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            Assert.Equal(0, res.res_2);
+        }
+    }
+}
