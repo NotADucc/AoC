967c141614348f0a2a288bd2489894a1f2c0c054
Guillaume Desktop <robertohelper@outlook.com>
2024-12-12 23:22:31 +0100

    feature: added ady 12

---
+++ Aoc2024/testing/Day12Tests.cs
@@ -0,0 +1,24 @@
+﻿namespace AoCTesting
+{
+    public class Day12Test
+    {
+        private Day11 proj = new();
+        private (long res_1, long res_2) res;
+        public Day12Test()
+        {
+            res = proj.Run();
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            Assert.Equal(1533644, res.res_1);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            Assert.Equal(936718, res.res_2);
+        }
+    }
+}
