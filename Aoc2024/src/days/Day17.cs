5561f0af6f91c0abab215d0724b4dc6ecea10c63
Guillaume Desktop <robertohelper@outlook.com>
2024-12-17 13:05:56 +0100

    feature: added aoc 17 base

---
+++ Aoc2024/src/days/Day17.cs
@@ -0,0 +1,14 @@
+using AoC;
+
+public class Day17 : IRun<long>
+{
+    public (long, long) Run()
+    {
+        string file_name = Path.Combine(Helper.GetInputFilesDir(), "aoc17.txt");
+        long res_1 = 0, res_2 = 0;
+
+
+        
+        return (res_1, res_2);
+    }
+}
---
+++ Aoc2024/testing/Day17Tests.cs
@@ -0,0 +1,24 @@
+﻿namespace AoCTesting
+{
+    public class Day17Test
+    {
+        private Day17 proj = new();
+        private (long res_1, long res_2) res;
+        public Day17Test()
+        {
+            res = proj.Run();
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            Assert.Equal(91464, res.res_1);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            Assert.Equal(494, res.res_2);
+        }
+    }
+}
