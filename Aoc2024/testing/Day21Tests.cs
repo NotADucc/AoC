5ed9bb1f23cc1d9f6c6e2969b47f47e634ab4f8d
Guillaume Desktop <robertohelper@outlook.com>
2024-12-22 00:57:29 +0100

    feature: ngl no clue how to do aoc 21

---
+++ Aoc2024/testing/Day21Tests.cs
@@ -0,0 +1,25 @@
+﻿using AoC.days;
+
+namespace AoCTesting;
+
+public class Day21Test
+{
+    private Day21 proj = new();
+    private (long res_1, long res_2) res;
+    public Day21Test()
+    {
+        res = proj.Run();
+    }
+
+    [Fact]
+    public void Part1()
+    {
+        Assert.Equal(222670, res.res_1);
+    }
+
+    [Fact]
+    public void Part2()
+    {
+        Assert.Equal(271397390297138, res.res_2);
+    }
+}
